261e03d726e923830e02d182b5a5726efda45a13
jaadisini <162589971+jaadisini@users.noreply.github.com>
2026-08-26 14:27:09 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,7 +38,7 @@ DURATION_LIMIT_MIN = int(
 
 
 # Proxy URL for YouTube/yt-dlp requests (leave empty to disable)
-PROXY_URL = getenv("PROXY_URL", "")
+PROXY_URL = getenv("PROXY_URL", "http://127.0.0.1:40000")
 
 
 # Duration Limit for downloading Songs in MP3 or MP4 format from bot
